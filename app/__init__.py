1d5015f38bd0b6685e6da1898e18e621c1fc7e4f
nimowairimu <nimowairimu25@gmail.com>
2021-03-08 22:13:12 +0300

    Create required folder structure to be used for the project .

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_login import LoginManager
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+    #....
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+# Initializing application
+app = Flask(__name__)
+
+
+
+from app.main import views
