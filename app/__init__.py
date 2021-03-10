65c5cd62df4568d6725f64c5b093fc74e90a6f4c
nimowairimu <nimowairimu25@gmail.com>
2021-03-10 23:31:54 +0300

    Add configurations into the application

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,10 @@
 from flask import Flask
-from flask_login import LoginManager
+from .config import DevConfig
 
-
-
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-
-def create_app(config_name):
-    #....
-    #Initializing Flask Extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
 # Initializing application
 app = Flask(__name__)
 
+# Setting up configuration
+app.config.from_object(DevConfig)
 
-
-from app.main import views
+from app import views
