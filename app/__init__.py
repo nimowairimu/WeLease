782ae55efa7f47c547968b77a0acba96f0da58c8
nimowairimu <nimowairimu25@gmail.com>
2021-03-11 03:49:09 +0300

    Update Changes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,35 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
-# Initializing application
-app = Flask(__name__)
+bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
+def create_app(config_name):
 
-from app import views
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///lease.db'
+
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # Will add the views and forms
+    from app.main import views
+    from app.main import errors
+
+    return app
+
---
+++ app/models.py
@@ -0,0 +1,25 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+
+
+class Lease(db.Model):
+  id = db.Column(db.Integer,primary_key=True)
+  name = db.Column(db.String,nullable=False)
+  size = db.Column(db.Integer,nullable=False)
+  location = db.Column(db.String(250),nullable=False)
+  cost = db.Column(db.Integer,nullable=False)
+  date_added = db.Column(db.DateTime,default=datetime.utcnow)
+
+  def __repr__(self):
+    return '<Location %r>' % self.id
+
+   
+  
